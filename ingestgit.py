551f4a900c390bbeeeaeeec5b53f49d5a80570f0
jackeldestripat <jackeldestripat@gmail.com>
2023-06-25 01:55:20 +0200

    Add arguments for ingestgit , and clean some code

--- ingestgit.py
+++ ingestgit.py
@@ -6,26 +6,20 @@ from dotenv import load_dotenv
 from multiprocessing import Pool
 from tqdm import tqdm
 
-from langchain.document_loaders import (
-    CSVLoader,
-    EverNoteLoader,
-    PyMuPDFLoader,
-    TextLoader,
-    UnstructuredEmailLoader,
-    UnstructuredEPubLoader,
-    UnstructuredHTMLLoader,
-    UnstructuredMarkdownLoader,
-    UnstructuredODTLoader,
-    UnstructuredPowerPointLoader,
-    UnstructuredWordDocumentLoader,
-    GitLoader
-)
+from langchain.document_loaders import  GitLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.docstore.document import Document
 from constants import CHROMA_SETTINGS
+import argparse
+
+def main():
+    # Parse the command line arguments
+    args = parse_arguments()
+    print(args.url)
+    print(args.name)
+    load(args.url, args.name)
 
 
 load_dotenv()
@@ -36,33 +30,45 @@ chunk_size = 500
 chunk_overlap = 50
 
 
-def load():
-    clone_url = "https://github.com/gradio-app/gradio"
+def load(url: str, name: str):
+    repo_path = "source_git/"+name
 
-    repo_path = "source_git/gradio-app"
-    #loader = GitLoader(repo_path, clone_url=clone_url)
-    loader = GitLoader(repo_path)
+    if not os.path.exists(repo_path):
+        loader = GitLoader(repo_path, clone_url=url)
+        documents = loader.load()
 
-    # Cargar los documentos del repositorio
-    documents = loader.load()
+        with Pool(processes=os.cpu_count()) as pool:
+            results = []
+            with tqdm(total=len(documents), desc='Loading new documents', ncols=80) as pbar:
+                for document in documents:
+                    results.extend(document)
+                    pbar.update()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+            texts = text_splitter.split_documents(documents)
 
-    with Pool(processes=os.cpu_count()) as pool:
-        results = []
-        with tqdm(total=len(documents), desc='Loading new documents', ncols=80) as pbar:
-            for document in documents:
-                results.extend(document)
-                pbar.update()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-        texts = text_splitter.split_documents(documents)
+            print(f"Appending to existing vectorstore at {persist_directory}")
+            embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
 
-        print(f"Appending to existing vectorstore at {persist_directory}")
-        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+            db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+            collection = db.get()
+            print(f"Creating embeddings. May take some minutes...")
+            db.add_documents(texts)
 
-        db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-        collection = db.get()
-        print(f"Creating embeddings. May take some minutes...")
-        db.add_documents(texts)
+        return results
+    else:
+        print("repo already exist")
 
-    return results
 
-load()
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Ingest a remote git repo')
+    parser.add_argument("--url", "-u", type=str, required=True,
+                        help='URL of the Git repository to ingest.')
+
+    parser.add_argument("--name", "-n", type=str, required=True,
+                        help='Name of the local repository.')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
